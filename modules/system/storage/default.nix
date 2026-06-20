c067a34739de5c43ed46e6f9e2d8809c0e548965
axosis <axosis.social357@gmail.com>
2026-06-21 05:22:43 +0545

    feat | mounting hdd in the home dir

--- modules/system/storage/default.nix
+++ modules/system/storage/default.nix
@@ -1,14 +1,14 @@
 { lib, config, ... }:
 let
-  cfg = config.systemSettings.dataSSD;
+  cfg = config.systemSettings.storage;
 in {
   options = {
-    systemSettings.dataSSD = {
+    systemSettings.storage = {
       enable = lib.mkEnableOption "Mount the DataSSD drive";
     };
   };
   config = lib.mkIf cfg.enable {
-    fileSystems."/mnt/DataSSD" = {
+    fileSystems."/home/axosis/DataSSD" = {
       device = "/dev/disk/by-uuid/3f9e646d-90e9-45b7-875b-f715d705689d";
       fsType = "ext4";
       options = [ "defaults" ];
