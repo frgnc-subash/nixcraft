0ac8dcc08b2b8fb1a73ad3974003b9eb7df7281b
axosis <axosis.social357@gmail.com>
2026-08-25 16:04:07 +0545

    remove the stylix input

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:nix-community/stylix";
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
