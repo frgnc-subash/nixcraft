1f58c76e9fd737de1669e90c0bdf9e13e81214d1
axosis <axosis.social357@gmail.com>
2026-09-11 07:07:31 +0545

    chore: flake update and dynamic colors

--- config/themes/dynamic/hyprland.lua
+++ config/themes/dynamic/hyprland.lua
@@ -1,6 +1,6 @@
 return {
-    outline = { colors = { "rgb(83dd00)", "rgb(afcfab)" }, angle = 45 },
-    outline_variant = "rgba(43493b66)",
-    primary = "rgb(83dd00)",
-    tertiary = "rgb(98d3b1)",
+    outline = { colors = { "rgb(00e47c)", "rgb(a4d0bc)" }, angle = 45 },
+    outline_variant = "rgba(3f494066)",
+    primary = "rgb(00e47c)",
+    tertiary = "rgb(8dd4c7)",
 }
